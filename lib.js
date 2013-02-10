9697e288911ded786cc9437b1397cbdf05512762
LeonFedotov <fedotov.leon@gmail.com>
2013-02-10 19:47:24 +0200

    change random

--- lib.js
+++ lib.js
@@ -1,7 +1,8 @@
 var _   = require('underscore');
 exports.randomArray = function(length) {
 	return _.range(length).map(function() {
-		return parseInt(Math.random()*length, 10);
+
+		return (parseInt(Math.random()*100, 10)+10)%100;
 	});
 };
 exports.arraySwap = function(arr, x, y) {
