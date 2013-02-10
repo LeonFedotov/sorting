090dc947a3e0c5de3952458be500c16b9530b5b3
LeonFedotov <fedotov.leon@gmail.com>
2013-02-10 17:43:49 +0200

    adding swap array helper

--- lib.js
+++ lib.js
@@ -4,3 +4,8 @@ exports.randomArray = function(length) {
 		return parseInt(Math.random()*length, 10);
 	});
 };
+exports.arraySwap = function(arr, x, y) {
+	var tmp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = tmp;
+};
